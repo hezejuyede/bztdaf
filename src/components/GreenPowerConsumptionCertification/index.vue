ac1041838a4c6ca36ba3414a5163fa2f58aeb333
qieerxi <1574408801@qq.com>
2022-08-19 14:52:44 +0800

    14点52分

--- src/components/GreenPowerConsumptionCertification/index.vue
+++ src/components/GreenPowerConsumptionCertification/index.vue
@@ -1,5 +1,24 @@
 <template>
     <div class="templateDiv">
+        <div class="templateDivTree fl">
+            <div class="TreeDiv">
+                <el-input
+                    style="width: 90%;margin: 10px 5% 10px 5%"
+                    placeholder="输入关键字进行过滤"
+                    v-model="filterText">
+                    <i slot="prefix" class="el-input__icon el-icon-search"></i>
+                </el-input>
+                <el-tree
+                    class="filter-tree"
+                    :data="data"
+                    :props="defaultProps"
+                    default-expand-all
+                    :filter-node-method="filterNode"
+                    ref="tree">
+                </el-tree>
+            </div>
+
+        </div>
         <div class="templateDivL fl">
             <div class="templateDivT">
                 <div class="templateDivTTop">
@@ -36,41 +55,16 @@
             </div>
         </div>
         <div class="templateDivR fl">
-            <div class="templateDivRL fl">
-                <div class="templateDivRLT">
-                    <div class="" style="margin-bottom: 5px">
-                        <i class="el-icon-s-unfold"></i>
-                    </div>
-                    <div class="">企</div>
-                    <div class="">业</div>
-                    <div class="">设</div>
-                    <div class="">备</div>
-                    <div class="">数</div>
-                </div>
-            </div>
             <div class="templateDivRR fl">
                 <div class="templateDivRRT">
-                    <div class="templateDivRRTL fl">
+                    <div class="templateDivRRTL">
+                        <div class="lineTitle">
+                            <div class=""></div>
+                            <div class="">企业耗电</div>
+                        </div>
                         <div id="lineList1" style="width:100%;height: 350px;"></div>
                     </div>
-                    <div class="templateDivRRTR fl">
-                        <el-table class="tb-edit" :data="tableData"
-                                  :header-cell-style="{background:'#EDF4F4',color:'#474F4F',height:'30px',borderColor:'#CAE5E4',fontSize:'12px',fontWeight: 'bold'}"
-                                  :cell-style="{fontSize:'12px',fontWeight: 'norma',color:'#444B4B',background:'#FFFFFF',borderColor:'#CAE5E4'}"
-                                  :height="400"
-                                  ref="moviesTable"
-                                  highlight-current-row style="width: 95%;margin: auto">
-                            <el-table-column label="时间" prop="time" align="center"></el-table-column>
-                            <el-table-column label="耗电量(MWH)" prop="MWH" align="center"></el-table-column>
-                            <el-table-column label="绿电占比(%)" prop="ldzb" align="center"></el-table-column>
-                        </el-table>
-                    </div>
-                </div>
-                <div class="templateDivRRT">
-                    <div class="templateDivRRTL fl">
-                        <div id="lineList2" style="width:100%;height: 350px;"></div>
-                    </div>
-                    <div class="templateDivRRTR fl">
+                    <div class="templateDivRRTR">
                         <el-table class="tb-edit" :data="tableData"
                                   :header-cell-style="{background:'#EDF4F4',color:'#474F4F',height:'30px',borderColor:'#CAE5E4',fontSize:'12px',fontWeight: 'bold'}"
                                   :cell-style="{fontSize:'12px',fontWeight: 'norma',color:'#444B4B',background:'#FFFFFF',borderColor:'#CAE5E4'}"
@@ -106,16 +100,55 @@ export default {
             count: "",
             tableData: [],
             treeData: [],
+
+            filterText: '',
+            data: [{
+                id: 1,
+                label: '邹平地区企业',
+                children: [{
+                    id: 4,
+                    label: '邹平XXXX公司',
+                }]
+            }, {
+                id: 2,
+                label: '沾化地区企业',
+                children: [{
+                    id: 5,
+                    label: '沾化XXXX公司'
+                }, {
+                    id: 6,
+                    label: '沾化XXXX公司'
+                }]
+            }, {
+                id: 3,
+                label: '滨城地区企业',
+                children: [{
+                    id: 7,
+                    label: '滨城XXXX公司'
+                }, {
+                    id: 8,
+                    label: '滨城XXXX公司'
+                }]
+            }],
+            defaultProps: {
+                children: 'children',
+                label: 'label'
+            }
+
+
         }
     },
 
 
     components: {GreenCardHistory, LabelPreview, shadinLayer},
-
+    watch: {
+        filterText(val) {
+            this.$refs.tree.filter(val);
+        }
+    },
     mounted() {
         this.getTree();
         this.setLine();
-        this.setLine2();
         this.getList();
     },
     created() {
@@ -232,109 +265,6 @@ export default {
             getListData();
         },
 
-        //设置饼图
-        setLine2() {
-
-            const getListData = async () => {
-                const result = await getLineList({})
-
-                var data = result.data.data.data;
-                let myChart = this.$echarts.init(document.getElementById('lineList2'));
-                myChart.clear();
-                myChart.setOption({
-                    tooltip: {
-                        trigger: 'axis'
-                    },
-                    legend: {
-                        data: ['低碳情景'],
-                        textStyle: {
-                            color: '#fff'
-                        },
-                        top: '8%'
-                    },
-                    grid: {
-                        top: '20%',
-                        left: '5%',
-                        right: '5%',
-                        bottom: '5%',
-                        containLabel: true
-                    },
-                    xAxis: {
-                        type: 'category',
-                        data: [2022.01, 2022.02, 2022.03, 2022.04, 2022.05, 2022.06, 2022.07, 2022.08, 2022.09, 2022.10, 2022.11, 2022.12],
-                        axisTick: { //X轴刻度线
-                            show: false,
-                        },
-                        boundaryGap: true,
-                        offset: 10,
-                        splitLine: {
-                            show: false
-                        },
-                        axisLine: {
-                            show: true,
-                            lineStyle: {
-                                color: "#259793",
-                                width: 2,
-                                type: "solid"
-                            }
-                        },
-                        axisLabel: {
-                            //  让x轴文字方向为竖向
-                            rotate: 30,
-                            textStyle: {
-                                color: '#9B9E9E'
-                            }
-                        }
-                    },
-                    yAxis: {
-                        type: 'value',
-                        name: "(电量:兆瓦时)",
-                        axisTick: { //y轴刻度线
-                            show: false,
-
-                        },
-                        axisLine: {
-                            show: false,
-                        },
-                        splitLine: {
-                            show: true,
-                        },
-                    },
-                    lineStyle: {
-                        color: '#86E6FD'
-                    },
-
-                    series: [
-                        {
-                            data: data,
-                            type: 'line',
-                            smooth: true,
-                            symbol: 'circle',
-                            symbolSize: 8,
-                            itemStyle: {
-                                normal: {
-                                    color: '#0ECDFC',
-                                    label: {
-                                        show: false,
-                                        color: '#FFF',
-                                        position: 'bottom',
-                                    },
-                                    areaStyle: {
-                                        type: 'default',
-                                        opacity: 0.1
-                                    }
-
-                                }
-                            },
-                        }
-                    ]
-                });
-            }
-            getListData();
-
-
-        },
-
 
         //获取树列表
         getTree() {
@@ -345,6 +275,12 @@ export default {
             }
             getListData();
         },
+
+
+        filterNode(value, data) {
+            if (!value) return true;
+            return data.label.indexOf(value) !== -1;
+        }
 
 
     }
@@ -361,6 +297,18 @@ export default {
     overflow: auto;
     background-color: #EDF4F4;
     padding-bottom: 50px;
+    .templateDivTree{
+        width: 20%;
+        height: 972px;
+        .TreeDiv{
+            width: 95%;
+            margin: 0 auto;
+            height: 100%;
+            padding-top: 20px;
+            background-color: #ffffff;
+        }
+
+    }
 
     .templateDivL {
         width: 40%;
@@ -417,57 +365,48 @@ export default {
     }
 
     .templateDivR {
-        width: 60%;
+        width: 39%;
         height: 972px;
-
-        .templateDivRL {
-            width: 7%;
-            height: 100%;
-            background-color: #ffffff;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            border-left: 10px solid #EDF4F4;
-
-            .templateDivRLT {
-                height: 95%;
-                width: 70%;
-                border-radius: 5px;
-                background-color: #EDF4F4;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                flex-direction: column;
-                color: #3C9D99;
-                font-weight: bold;
-                font-size: 12px;
-                cursor: pointer;
-            }
-
-        }
-
+        margin-left: 1%;
         .templateDivRR {
             background-color: #ffffff;
-            width: 93%;
+            width: 100%;
             height: 972px;
-
             .templateDivRRT {
                 width: 100%;
                 height: 485px;
                 padding-top: 50px;
 
                 .templateDivRRTL {
-                    width: 60%;
+                    width: 100%;
                     height: 100%;
                 }
 
                 .templateDivRRTR {
-                    width: 40%;
+                    width: 100%;
                     height: 100%;
                 }
 
             }
         }
+    }
+
+    .lineTitle{
+        width: 100%;
+        height: 40px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        div:first-child{
+            width:10px;
+            height: 10px;
+            border-radius: 50%;
+            background-color:#09CCFC;
+            margin-right: 5px;
+            margin-top: 2.5px;
+
+        }
+
     }
 
 
