5ccedd10f9b967d3b924b1bea19bb30339127161
qieerxi <1574408801@qq.com>
2022-08-19 15:53:15 +0800

    15点53分

--- src/components/GreenPowerConsumptionCertification/components/LabelPreview.vue
+++ src/components/GreenPowerConsumptionCertification/components/LabelPreview.vue
@@ -5,10 +5,9 @@
                 <el-descriptions-item label="-滞纳方"   :labelStyle="{'color': '#3C937E','font-weight':'bold'}">山东电力交易中心</el-descriptions-item>
                 <el-descriptions-item label="-滞纳地"   :labelStyle="{'color': '#3C937E','font-weight':'bold'}">滨州市</el-descriptions-item>
                 <el-descriptions-item label="-滞纳周期"  :labelStyle="{'color': '#3C937E','font-weight':'bold'}">2021年9月1号-2022年9月2号</el-descriptions-item>
-                <el-descriptions-item label="-供电方"   :labelStylee="{'color': '#3C937E','font-weight':'bold'}">山东电力交易中心</el-descriptions-item>
-                <el-descriptions-item label="-输电方"   :labelStyle="{'color': '#3C937E','font-weight':'bold'}">山东电力交易中心</el-descriptions-item>
+                <el-descriptions-item label="-供电方"   :labelStyle="{'color': '#3C937E','font-weight':'bold'}">山东电力交易中心</el-descriptions-item>
                 <el-descriptions-item label="-消纳电量"   :labelStyle="{'color': '#3C937E','font-weight':'bold'}">700000MWH</el-descriptions-item>
-                <el-descriptions-item label="-电量类型"   :labelStyle="{'color': '#3C937E','font-weight':'bold'}">恒定电流</el-descriptions-item>
+                <el-descriptions-item label="-认证周期"   :labelStyle="{'color': '#3C937E','font-weight':'bold'}">1天</el-descriptions-item>
             </el-descriptions>
         </div>
         <div class="templateDivR fl">
