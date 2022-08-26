e3cff6da46134f659678f200608cdc0c63ce5935
qieerxi <1574408801@qq.com>
2022-08-26 12:34:05 +0800

    26号12点23分

--- src/components/TransactionContractAnalysis/components/index.vue
+++ src/components/TransactionContractAnalysis/components/index.vue
@@ -2,8 +2,8 @@
     <div class="greenPower-parent">
         <div style="height:100%;width: 100%;position: relative">
             <div id="chart_1" style="height: 100%;width: 100%;"></div>
-            <div class="fountStyle" style="top: 12%;left: 1%;">山东沾化奥仕化学有限公司</div>
-            <div class="fountStyle" style="top: 82%;left: 1%;">东立绿电聚合代理商</div>
+            <div class="fountStyle" style="top: 8%;left: 1%;">山东沾化奥仕化学有限公司</div>
+            <div class="fountStyle" style="top: 97%;left: 1%;">东立绿电聚合代理商</div>
         </div>
     </div>
 </template>
